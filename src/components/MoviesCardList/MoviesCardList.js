1fd5fe3ffb30860e9a85b71c031c15064faf26d2
delioncourts <delioncourtles@yandex.ru>
2022-08-27 14:47:53 +0300

    upd MoviesCardList

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,15 +4,41 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 import './MoviesCardList.css';
 
 
-function MoviesCardList({ cards }) {
+function MoviesCardList({
+    movies,
+    savedMovies,
+    onSaveMovie,
+    onDeleteMovie,
+    isLoading,
+    isSearchDone,
+    onRenderMovies,
+    moreLoading
+}) {
+
+    const loadingButton = moreLoading ? `movieCardList__button` : `movieCardList__button-hidden`;
+
     return (
         <section className='movieCardList'>
             <div className='movieCardList__container'>
-                {cards.map((card) => (
-                    <MoviesCard key={card.id} card={card} />
+                {movies.map((movie) => (
+                    <MoviesCard
+                        movie={movie}
+                        key={movie._id || movie.id}
+                        savedMovies={savedMovies}
+                        onSaveMovie={onSaveMovie}
+                        onDeleteMovie={onDeleteMovie} />
                 ))}
             </div>
-            <button className='movieCardList__button' aria-label='Загрузить ещё' type='button'>Ещё</button>
+            {!isLoading ? isSearchDone
+                ? <div className='movieCardList__button'>
+                    <button onClick={onRenderMovies}
+                        className={loadingButton}
+                        aria-label='Загрузить ещё'
+                        type='button'>Ещё</button>
+                </div>
+                : ("")
+                : ("")
+            }
         </section>
     );
 };
